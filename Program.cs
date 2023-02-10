4e630a74f53b55c30f9979fca66be13c736a4147
Berit Carvalho <beritcarvalho@gmail.com>
2023-02-10 19:20:31 -0300

    "Ajuste na mensagem"

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Consegui publicar minha aplicação!");
 
 app.Run();
